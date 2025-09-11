766a7a81e6f5b7f1c0a46128667957d4f3ebbf70
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-11 14:20:32 +0200

    test_RevertWhen_RequestRedeemNotAuthorized

--- test/MagmaAsyncModule.t.sol
+++ test/MagmaAsyncModule.t.sol
@@ -8,7 +8,7 @@ import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {WrappedMonad} from "monad/WrappedMonad.sol";
 import {MagmaBase} from "src/MagmaBase.sol";
-import {ErrZeroAmount, ErrRequestPending, ErrZeroShares} from "src/MagmaErrorsModule.sol";
+import {ErrZeroAmount, ErrRequestPending, ErrZeroShares, ErrNotAuthorized} from "src/MagmaErrorsModule.sol";
 
 contract MagmaAsyncModuleTest is BaseTest {
     function setUp() public override {
@@ -394,7 +394,12 @@ contract MagmaAsyncModuleTest is BaseTest {
     function test_RevertWhen_RequestRedeem0Shares() public {
         vm.prank(user);
         vm.expectRevert(ErrZeroShares.selector);
-        assertEq(0, magma.requestRedeem(0, user, user));
+        magma.requestRedeem(0, user, user);
+    }
+
+    function test_RevertWhen_RequestRedeemNotAuthorized() public {
+        vm.expectRevert(ErrNotAuthorized.selector);
+        magma.requestRedeem(5, user, user);
     }
 
     // function test_OperatorApproval() public {
