5e68bcee30c6f1016da0de7b558fef91b87f9e5f
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-11 18:53:33 +0200

    introduce reverts in completeUserWithdrawal CoreVault.sol in case of transfer or withdrawal fail

--- src/CoreVault.sol
+++ src/CoreVault.sol
@@ -398,20 +398,19 @@ contract CoreVault is
                 // Mark withdrawal ID as completed
                 _markWithdrawalCompleted(_valId, _withdrawalId);
             } else {
-                emit WithdrawalFailed(_valId, _withdrawalId);
+                // TODO: test this edge case
+                revert ErrWithdrawalFailed(_valId, _withdrawalId);
             }
         }
 
         // Send all accumulated ETH to user in a single transaction
         if (_totalSuccessfulWithdrawals > 0) {
-            (bool success,) = _user.call{value: _totalSuccessfulWithdrawals}("");
-            if (success) {
-                _totalWithdrawn = _totalSuccessfulWithdrawals;
-            } else {
-                // If the single payment fails, emit failure event
-                emit WithdrawalPaymentFailed(0, 0, _user, _totalSuccessfulWithdrawals);
-                _totalWithdrawn = 0;
+            (bool success,) = address(magma).call{value: _totalSuccessfulWithdrawals}("");
+            if (!success) {
+                // TODO: test this edge case
+                revert ErrNativeTransferFailed();
             }
+            _totalWithdrawn = _totalSuccessfulWithdrawals;
         }
 
         // Clear all withdrawal requests for this user after processing
