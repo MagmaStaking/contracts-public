51463e380d841a1bc61c412346264e6cc0438249
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-22 15:52:49 +0200

    add _totalWithdrawnAfterFee to completeUserWithdrawal in gVault

--- src/gVault.sol
+++ src/gVault.sol
@@ -211,7 +211,11 @@ contract gVault is Initializable, UUPSUpgradeable, ReentrancyGuardUpgradeable, I
         }
     }
 
-    function completeUserWithdrawal(address _user) external nonReentrant returns (uint256 _totalWithdrawn) {
+    function completeUserWithdrawal(address _user)
+        external
+        nonReentrant
+        returns (uint256 _totalWithdrawn, uint256 _totalWithdrawnAfterFee)
+    {
         return _completeUserWithdrawal(_user);
     }
 
