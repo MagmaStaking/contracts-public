8632db401aaf2f4f801b45a2a5000ab307788435
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-23 18:41:30 +0200

    add indexed events IGVault

--- interfaces/IGVault.sol
+++ interfaces/IGVault.sol
@@ -38,15 +38,15 @@ interface IGVault is IBaseVault {
     function validatorCap(uint64 valId) external view returns (uint256);
     function defaultCapBps() external view returns (uint256);
 
-    event PositionUpdated(address indexed user, uint64 indexed valId, uint256 amount, bool isDelegate);
-    event CapChanged(uint64 indexed valId, uint256 newCap);
-    event DefaultCapUpdated(uint256 newDefaultBps);
+    event PositionUpdated(address indexed user, uint64 indexed valId, uint256 indexed amount, bool isDelegate);
+    event CapChanged(uint64 indexed valId, uint256 indexed newCap);
+    event DefaultCapUpdated(uint256 indexed newDefaultBps);
     // Rebalance admin events
-    event AdminInitiatedRebalance(uint16 bps);
-    event AdminCompletedRebalance(uint256 amountForwarded);
-    event AdminCompletedRebalanceWithdrawal(uint64 indexed valId, uint256 amount);
+    event AdminInitiatedRebalance(uint16 indexed bps);
+    event AdminCompletedRebalance(uint256 indexed amountForwarded);
+    event AdminCompletedRebalanceWithdrawal(uint64 indexed valId, uint256 indexed amount);
 
-    event ProcessedBatch(uint64 indexed valId, uint8 withdrawalId, uint256 amount);
+    event ProcessedBatch(uint64 indexed valId, uint8 indexed withdrawalId, uint256 indexed amount);
 
     // User withdrawal distribution events
     event WithdrawalAmountMismatch(
