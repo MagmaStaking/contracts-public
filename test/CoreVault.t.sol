2840b5f6cebb59375939d5ed83ba9bb9ab11dad2
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-23 12:12:45 +0200

    fix CoreVault

--- test/CoreVault.t.sol
+++ test/CoreVault.t.sol
@@ -13,7 +13,7 @@ contract CoreVaultTest is BaseTest {
         // Redeploy CoreVault with epochSeconds = 0 to bypass epoch guard for this unit test
         address coreImpl = address(new CoreVault());
         address coreProxy = UnsafeUpgrades.deployUUPSProxy(
-            coreImpl, abi.encodeCall(CoreVault.initialize, (address(magma), uint256(0), uint256(0)))
+            coreImpl, abi.encodeCall(CoreVault.initialize, (address(magma), uint256(0)))
         );
         coreVault = CoreVault(payable(coreProxy));
         // Wire magma to new coreVault
