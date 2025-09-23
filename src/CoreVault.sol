9d40a4fe234f48e4fac4da7091e54d7800b24890
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-23 13:26:12 +0200

    add addValidators and setMaxValidatorPerBatch to CoreVault

--- src/CoreVault.sol
+++ src/CoreVault.sol
@@ -25,6 +25,8 @@ contract CoreVault is
 
     uint256 public epochSeconds;
 
+    uint64 private _maxValidatorPerBatch;
+
     // Rebalance pacing guard
     uint256 public lastRebalanceTimestamp;
 
@@ -42,12 +44,13 @@ contract CoreVault is
         return super.paused();
     }
 
-    function initialize(address _magma, uint256 _epochSeconds) external initializer {
+    function initialize(address _magma, uint256 _epochSeconds, uint64 maxValidatorPerBatch_) external initializer {
         __ReentrancyGuard_init();
         __Pausable_init();
         __VaultBase_init(_magma);
         epochSeconds = _epochSeconds;
         finishedLastRebalance = true;
+        _maxValidatorPerBatch = maxValidatorPerBatch_;
     }
 
     // Accept native funds returned from precompile withdrawals
@@ -82,6 +85,20 @@ contract CoreVault is
      */
     function addValidator(uint64 _valId) external onlyAdmin onlyAfterEpoch {
         _registerValidator(_valId);
+        _redelegateInitiate();
+    }
+
+    /**
+     * @notice Step 1: Add validators and initiate rebalance phase 1 (undelegation)
+     * @dev Add validators and trigger excess undelegation. Redistribution must be done manually via redistributeToValidators()
+     * @param validators The array of validator IDs to add
+     */
+    function addValidators(uint64[] memory validators) external onlyAdmin onlyAfterEpoch {
+        if (validators.length > _maxValidatorPerBatch) revert MaxValidators(_maxValidatorPerBatch);
+
+        for (uint256 i = 0; i < validators.length; i++) {
+            _registerValidator(validators[i]);
+        }
         _redelegateInitiate();
     }
 
@@ -501,6 +518,10 @@ contract CoreVault is
      */
     function getUserWithdrawalRequestCount(address _user) external view returns (uint256) {
         return userWithdrawalRequests[_user].length;
+    }
+
+    function setMaxValidatorPerBatch(uint64 maxValidatorPerBatch) external onlyAdmin {
+        _maxValidatorPerBatch = maxValidatorPerBatch;
     }
 
     function _authorizeUpgrade(address) internal view override {
