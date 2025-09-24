aaf3c93e7f90bd388e86619926ed40a9f8472e47
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-24 12:48:58 +0200

    remove unused variables GVaultRewardsTest

--- test/GVaultFees.t.sol
+++ test/GVaultFees.t.sol
@@ -83,7 +83,7 @@ contract GVaultRewardsTest is BaseTest {
         uint256 shares = magma.balanceOf(user);
         uint256 sharesToRedeem = shares / 20;
         vm.prank(user);
-        uint256 requestId = magma.requestRedeemGVault(sharesToRedeem, user, user, VAL_1);
+        magma.requestRedeemGVault(sharesToRedeem, user, user, VAL_1);
 
         // Wait for async delay and withdrawal maturity in the mock
         vm.warp(block.timestamp + magma.redeemDelay());
