c84c0dea98eb87722386d63cdfd95743b74519a8
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-24 16:59:46 +0200

    initialize params in coreVaultUndelegationSimple test struct

--- test/CoreVaultUndelegationSimple.t.sol
+++ test/CoreVaultUndelegationSimple.t.sol
@@ -54,7 +54,18 @@ contract CoreVaultUndelegationSimpleTest is Test {
             magmaImpl,
             abi.encodeCall(
                 Magma.initialize,
-                (IERC20(address(wmon)), "gMON", "gMON", admin, address(0), address(0), 10, 0, admin, uint256(0))
+                Magma.InitializeParams({
+                    asset: IERC20(address(wmon)),
+                    name: "gMON",
+                    symbol: "gMON",
+                    admin: admin,
+                    coreVault: address(0),
+                    gVault: address(0),
+                    rewardsFee: 10,
+                    withdrawalFee: 0,
+                    feeReceiver: admin,
+                    redeemDelay: uint256(0)
+                })
             )
         );
         magma = Magma(payable(magmaProxy));
