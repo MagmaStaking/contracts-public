e6985f1dc4ec620c2d9470f6e73c0977fd252b95
ferrodri <f.rodriguez.hervias@gmail.com>
2025-09-25 14:21:44 +0200

    duplicated-imports

--- src/Magma.sol
+++ src/Magma.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.30;
 import {WrappedMonad} from "../monad/WrappedMonad.sol";
 import {
     ErrNotEnoughAssetsGVault,
-    ErrZeroAddress,
     ErrRequestPending,
     ErrZeroShares,
     ErrNotAuthorized,
